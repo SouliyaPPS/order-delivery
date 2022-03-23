bf543a3ffcb6701f0d0e2f1c9ca54459c54e1081
Souliya PPS <souliyappsdev@gmail.com>
2022-03-23 19:49:45 +0700

    main

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,130 +1,323 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function NavLink({ to, children }) {
-  return (
-    <a href={to} className={`mx-4`}>
-      {children}
-    </a>
-  );
-}
+// function NavLink({ to, children }) {
+//   return (
+//     <a href={to} className={`mx-4`}>
+//       {children}
+//     </a>
+//   );
+// }
 
-function MobileNav({ open, setOpen }) {
+// function MobileNav({ open, setOpen }) {
+//   return (
+//     <div
+//       className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${
+//         open ? "-translate-x-0" : "-translate-x-full"
+//       } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
+//     >
+//       <div className="flex items-center justify-center h-18 bg-amber-500 filter drop-shadow-md">
+//         {" "}
+//         {/*logo container*/}
+//         <div>
+//           <button>
+//             <img
+//               className="w-auto h-12"
+//               src="https://i.ibb.co/QMMM7T1/unnamed.png"
+//               alt=""
+//             />
+//           </button>
+//         </div>
+//       </div>
+//       <div className="flex flex-col ml-4">
+//         <a
+//           className="my-4 text-xl font-medium"
+//           href="/about"
+//           onClick={() =>
+//             setTimeout(() => {
+//               setOpen(!open);
+//             }, 100)
+//           }
+//         >
+//           About
+//         </a>
+//         <a
+//           className="my-4 text-xl font-normal"
+//           href="/contact"
+//           onClick={() =>
+//             setTimeout(() => {
+//               setOpen(!open);
+//             }, 100)
+//           }
+//         >
+//           Contact
+//         </a>
+//         <div className="flex flex-col">
+//           <a
+//             className="my-4 text-xl font-normal"
+//             href="https://goo.gl/maps/7pr2KsuYbiAHPGNk8"
+//             onClick={() =>
+//               setTimeout(() => {
+//                 setOpen(!open);
+//               }, 100)
+//             }
+//           >
+//             Locations
+// <button className="px-1 w-1/1 sm:w-1/1">
+//   <img
+//     className="w-auto h-5"
+//     src="https://i.ibb.co/j3L3M1z/icons8-location.gif"
+//     alt=""
+//   />
+// </button>
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default function Navbar() {
+//   const [open, setOpen] = useState(false);
+//   return (
+//     <nav className="flex items-center px-4 py-4 h-14 bg-amber-500 filter drop-shadow-md">
+//       <MobileNav open={open} setOpen={setOpen} />
+// <div className="flex items-center w-3/12">
+//   <div>
+//     <button>
+//       <img
+//         className="w-auto h-12"
+//         src="https://i.ibb.co/QMMM7T1/unnamed.png"
+//         alt=""
+//       />
+//     </button>
+//   </div>
+// </div>
+//       <div className="flex items-center justify-end w-9/12">
+//         <div
+//           className="relative z-50 flex flex-col items-center justify-between w-8 h-8 md:hidden"
+//           onClick={() => {
+//             setOpen(!open);
+//           }}
+//         >
+//           {/* hamburger button */}
+//           <span
+//             className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+//               open ? "rotate-45 translate-y-3.5" : ""
+//             }`}
+//           />
+//           <span
+//             className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${
+//               open ? "w-0" : "w-full"
+//             }`}
+//           />
+//           <span
+//             className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+//               open ? "-rotate-45 -translate-y-3.5" : ""
+//             }`}
+//           />
+//         </div>
+
+//         <div className="hidden md:flex">
+//           <NavLink to="/contact">CONTACT</NavLink>
+//           <NavLink to="/about">ABOUT</NavLink>
+//           <NavLink to="https://goo.gl/maps/7pr2KsuYbiAHPGNk8">
+//             LOCATION 🗺️📌
+//           </NavLink>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+import { Link } from "react-scroll";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div
-      className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${
-        open ? "-translate-x-0" : "-translate-x-full"
-      } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
-    >
-      <div className="flex items-center justify-center h-18 bg-amber-500 filter drop-shadow-md">
-        {" "}
-        {/*logo container*/}
-        <div>
-          <button>
-            <img
-              className="w-auto h-12"
-              src="https://i.ibb.co/QMMM7T1/unnamed.png"
-              alt=""
-            />
-          </button>
+    <div>
+      <nav className=" fixed w-full z-10 ">
+        <div className="w-full flex items-center px-4 py-4 h-14">
+          <div className="flex items-center h-20 w-full">
+            <div className="flex items-center ml-0 justify-between w-full">
+              <div className="flex justify-center  items-center flex-shrink-0 ">
+                <h1 className=" font-bold text-xl cursor-pointer">
+                  <div className="flex items-center w-13/12">
+                    {/* <div>
+                      <button>
+                        <img
+                          className="w-auto h-12"
+                          src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/sIL81wOFbRA8cH4Q2JDw.png"
+                          alt=""
+                        />
+                      </button>
+                    </div> */}
+                  </div>
+                </h1>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <Link
+                    activeClass="Home"
+                    to="about"
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    className="cursor-pointer hover:bg-amber-500 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    activeClass="about"
+                    to="about"
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    className="cursor-pointer hover:bg-amber-500 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    About
+                  </Link>
+
+                  <Link
+                    activeClass="contact"
+                    to="contact"
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    className="cursor-pointer hover:bg-amber-500 text-black hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Contact
+                    {/* <button className="px-1 w-1/1 sm:w-1/1">
+                      <img
+                        className="w-auto h-4"
+                        src="https://i.ibb.co/j3L3M1z/icons8-location.gif"
+                        alt=""
+                      />
+                    </button> */}
+                  </Link>
+
+                  <Link
+                    activeClass="work"
+                    to="work"
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    // className="w-1/1 sm:w-1/1 cursor-pointer hover:bg-amber-500 text-black hover:text-white px-4 py-2 rounded-md text-sm font-medium"
+                  ></Link>
+                </div>
+              </div>
+            </div>
+            <div className="mr-0 flex md:hidden ">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-amber-500 inline-flex items-center justify-center p-2 rounded-md text-white  hover:bg-amber-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-amber-500 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col ml-4">
-        <a
-          className="my-4 text-xl font-medium"
-          href="/about"
-          onClick={() =>
-            setTimeout(() => {
-              setOpen(!open);
-            }, 100)
-          }
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
         >
-          About
-        </a>
-        <a
-          className="my-4 text-xl font-normal"
-          href="/contact"
-          onClick={() =>
-            setTimeout(() => {
-              setOpen(!open);
-            }, 100)
-          }
-        >
-          Contact
-        </a>
-        <div className="flex flex-col">
-          <a
-            className="my-4 text-xl font-normal"
-            href="https://goo.gl/maps/7pr2KsuYbiAHPGNk8"
-            onClick={() =>
-              setTimeout(() => {
-                setOpen(!open);
-              }, 100)
-            }
-          >
-            Locations
-            <button className="px-1 w-1/1 sm:w-1/1">
-              <img
-                className="w-auto h-5"
-                src="https://i.ibb.co/j3L3M1z/icons8-location.gif"
-                alt=""
-              />
-            </button>
-          </a>
-        </div>
-      </div>
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div
+                ref={ref}
+                className="bg-white px-2 pt-2 pb-3 space-y-1 sm:px-3"
+              >
+                <Link
+                  href="/home"
+                  activeClass="home"
+                  to="home"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer hover:bg-amber-500 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </Link>
+                <Link
+                  href="/about"
+                  activeClass="about"
+                  to="about"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer hover:bg-amber-500 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  About
+                </Link>
+
+                <Link
+                  href="/contact"
+                  activeClass="work"
+                  to="work"
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  className="cursor-pointer hover:bg-amber-500 text-black hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Contact
+                  <button className="px-1 w-1/1 sm:w-1/1">
+                    <img
+                      className="w-auto h-4"
+                      src="https://i.ibb.co/j3L3M1z/icons8-location.gif"
+                      alt=""
+                    />
+                  </button>
+                </Link>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
     </div>
   );
 }
 
-export default function Navbar() {
-  const [open, setOpen] = useState(false);
-  return (
-    <nav className="flex items-center px-4 py-4 h-14 bg-amber-500 filter drop-shadow-md">
-      <MobileNav open={open} setOpen={setOpen} />
-      <div className="flex items-center w-3/12">
-        <div>
-          <button>
-            <img
-              className="w-auto h-12"
-              src="https://i.ibb.co/QMMM7T1/unnamed.png"
-              alt=""
-            />
-          </button>
-        </div>
-      </div>
-      <div className="flex items-center justify-end w-9/12">
-        <div
-          className="relative z-50 flex flex-col items-center justify-between w-8 h-8 md:hidden"
-          onClick={() => {
-            setOpen(!open);
-          }}
-        >
-          {/* hamburger button */}
-          <span
-            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
-              open ? "rotate-45 translate-y-3.5" : ""
-            }`}
-          />
-          <span
-            className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${
-              open ? "w-0" : "w-full"
-            }`}
-          />
-          <span
-            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
-              open ? "-rotate-45 -translate-y-3.5" : ""
-            }`}
-          />
-        </div>
-
-        <div className="hidden md:flex">
-          <NavLink to="/contact">CONTACT</NavLink>
-          <NavLink to="/about">ABOUT</NavLink>
-          <NavLink to="https://goo.gl/maps/7pr2KsuYbiAHPGNk8">
-            LOCATION 🗺️📌
-          </NavLink>
-        </div>
-      </div>
-    </nav>
-  );
-}
+export default Navbar;
--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from "react-icons/ai";
-import MobileNav from "./Navbar";
-import Search from "./Search";
 
 const featuredProducts = [
-  "https://scontent.fvte2-3.fna.fbcdn.net/v/t39.30808-6/274356861_148182434277389_629341167391831257_n.png?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeFgzqgWUvbDP0qUZaP8tGW4v55AJIyQ1-W_nkAkjJDX5QXGZgs5eIG3TCec9cDx1ZidYT2Jl1CCGuq54DZl582D&_nc_ohc=jdBqORp0NMoAX8pzPiw&_nc_ht=scontent.fvte2-3.fna&oh=00_AT-Af2ckZvYlVeUJkJYn5dmvY03ED4D5UgblYPWOTBcmFw&oe=623F5447",
-  "https://scontent.fvte2-2.fna.fbcdn.net/v/t39.30808-6/271603102_138679871894312_2526127250027106159_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeEy7eSOYbagb4nX25dfMPlbP7Xdk2ctajQ_td2TZy1qNBOTSnbrJbaSbDPFiGGRMdfWbo_0XTXhz4rUgZuRirQY&_nc_ohc=IRKmm2jHTCYAX9ZG8Z7&_nc_ht=scontent.fvte2-2.fna&oh=00_AT_LQGS9p5_aaZKlUPn-6RMSf4xop1TP7Upo8MHYUiL-Zw&oe=623F5A00",
-  "https://scontent.fvte2-1.fna.fbcdn.net/v/t39.30808-6/275302965_151653650596934_9212390331757319191_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeEyD1V4RC9ERFpPDQn4JksOT71fdU74XY1PvV91TvhdjQLGnHxZA1ve_z6qo6T5d6k47hTK_8lz2kHPuu8MRfsW&_nc_ohc=2KBC6_oFFAoAX-3No4f&tn=FK-vvCEyh36FmkOo&_nc_ht=scontent.fvte2-1.fna&oh=00_AT_bP1rKEPW_LFm6IksHPr9WAFGQ-TcjaGPfxDIjcRbYQQ&oe=623ED3CF",
+  // "https://scontent.fvte2-3.fna.fbcdn.net/v/t39.30808-6/274356861_148182434277389_629341167391831257_n.png?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeFgzqgWUvbDP0qUZaP8tGW4v55AJIyQ1-W_nkAkjJDX5QXGZgs5eIG3TCec9cDx1ZidYT2Jl1CCGuq54DZl582D&_nc_ohc=jdBqORp0NMoAX8pzPiw&_nc_ht=scontent.fvte2-3.fna&oh=00_AT-Af2ckZvYlVeUJkJYn5dmvY03ED4D5UgblYPWOTBcmFw&oe=623F5447",
+  "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/O0aAtUshbNp6U3kPN6rR.png",
+  "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/O0aAtUshbNp6U3kPN6rR.png",
+  "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/O0aAtUshbNp6U3kPN6rR.png",
 ];
 
 let count = 0;
@@ -53,31 +52,27 @@ export default function Slider() {
     setCurrentIndex(count);
     slideRef.current.classList.add("fade-anim");
   };
-  const [open, setOpen] = useState(false);
 
   return (
     <div ref={slideRef} className="relative w-full select-none">
-      <nav>
-        <div className="absolute flex items-center justify-between w-full px-3 transform top-1/2">
-          <button
-            className="p-1 text-white transition bg-black bg-opacity-50 rounded-full cursor-pointer hover:bg-opacity-100"
-            onClick={handleOnPrevClick}
-          >
-            <AiOutlineVerticalRight size={30} />
-          </button>
-          <button
-            className="p-1 text-white transition bg-black bg-opacity-50 rounded-full cursor-pointer hover:bg-opacity-100"
-            onClick={handleOnNextClick}
-          >
-            <AiOutlineVerticalLeft size={30} />
-          </button>
-        </div>
-        <MobileNav open={open} setOpen={setOpen} />
-        <div className="aspect-w-20 aspect-h-10">
-          <img src={featuredProducts[currentIndex]} alt="" />
-        </div>
-        <Search />
-      </nav>
+      {/* <div className="absolute flex items-center justify-between w-full px-3 transform top-1/2">
+        <button
+          className="p-1 text-white transition bg-black bg-opacity-50 rounded-full cursor-pointer hover:bg-opacity-100"
+          onClick={handleOnPrevClick}
+        >
+          <AiOutlineVerticalRight size={30} />
+        </button>
+        <button
+          className="p-1 text-white transition bg-black bg-opacity-50 rounded-full cursor-pointer hover:bg-opacity-100"
+          onClick={handleOnNextClick}
+        >
+          <AiOutlineVerticalLeft size={30} />
+        </button>
+      </div> */}
+
+      <div className="aspect-w-20 aspect-h-10">
+        <img src={featuredProducts[currentIndex]} alt="" />
+      </div>
     </div>
   );
 }
