66404886f279cb3cc3cabc099992a222b5300092
Souliya PPS <souliyappsdev@gmail.com>
2022-03-25 16:12:03 +0700

    main

--- pages/ShopNow.js
+++ pages/ShopNow.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Tabs from "../components/Tabs";
+// import BottomNavigation from "../components/BottomNavigation";
 
 const ShopNow = () => {
   return (
     <div>
       <Navbar />
+      {/* <BottomNavigation /> */}
       <Tabs />
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,39 @@
+// import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import PropTypes from "prop-types";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import createEmotionCache from "../utility/createEmotionCache";
+import lightTheme from "../styles/theme/lightTheme";
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
+const clientSideEmotionCache = createEmotionCache();
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ChakraProvider>
+//       <Component {...pageProps} />
+//     </ChakraProvider>
+//   );
+// }
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
-}
+};
 
 export default MyApp;
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
