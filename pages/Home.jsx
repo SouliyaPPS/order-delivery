99f86fa63a0960561ec745e72955259a58ecacab
Souliya PPS <souliyappsdev@gmail.com>
2022-04-06 21:31:44 +0700

    main

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Tabs from "../components/Tabs";
 import ListProducts from "./ListProducts";
 import styles from "../styles/CartProducts.module.css";
+import CartProductstyles from "../styles/CartProducts.module.css";
 
 function Home() {
   return (
@@ -14,7 +15,7 @@ function Home() {
         alt="Landscape picture"
         width={500}
         height={500}
-        className="mx-auto z-auto"
+        className="mx-auto"
       />
       <img
         src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/XOv6nw7rmZBgT8fMWlY9.png"
@@ -23,134 +24,136 @@ function Home() {
         height={500}
         className="mx-auto"
       />
-      <div className={styles.ListProducts}>
-        <ListProducts />
+
+      <div className={CartProductstyles.HomeWidth}>
+        <div className={styles.ListProducts}>
+          <ListProducts />
+        </div>
+        <div>
+          <h6 className="my-px px-1 sm:px-1 pb-10 "></h6>
+        </div>
+
+        <img
+          src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/Em5mtzT5uYe5Fea8BORk.jpg"
+          alt="Landscape picture"
+          width={500}
+          height={500}
+          className="mx-auto"
+        />
+
+        <div>
+          <h1 className="text-lg my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black to-pink-600 font-bold">
+            <h4 align="center">One support, One reduce</h4>
+          </h1>
+        </div>
+        <div>
+          <h6 className="my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black text-justify mx-auto">
+            Let is reduce the fabric scrap from the craft production in Laos by
+            supporting the Re-craft products made from fabric scrap and everyone
+            also can promote reuse and recycling to society by sharing your
+            picture with Re-Craft products on social media.
+          </h6>
+        </div>
+        <br></br>
+        <div>
+          <h1 className="text-lg my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black to-pink-600 font-bold justify-center">
+            <h4 align="center">STAY CONNECTED</h4>
+          </h1>
+        </div>
+        <div>
+          <h6 className="my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black text-justify mx-auto">
+            <h4 align="center">Join our community</h4>
+          </h6>
+        </div>
+
+        <div>
+          <h6 className="my-px px-1 sm:px-1 pb-1"></h6>
+        </div>
+
+        <div className="flex grid-cols-4 mx-auto flex-wrap justify-center">
+          <div className="rounded-md">
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/6hs76UNVSk5usaPNztss.png"
+                alt="Facebook"
+                width={70}
+                height={70}
+                className="mx-auto "
+              />
+              <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
+                <h4 align="center">Facebook </h4>
+              </h1>
+            </a>
+          </div>
+
+          <div className="rounded-md">
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/SiVGiQntQs6w9IfKJOrU.png"
+                alt="Instagram"
+                width={70}
+                height={70}
+                className="mx-auto"
+              />
+              <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
+                <h4 align="center">Instagram</h4>
+              </h1>
+            </a>
+          </div>
+
+          <div className="rounded-md">
+            <a
+              href="https://wa.me/+8562023564154"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/2PQrdSjHEah2N7wM4oGQ.png"
+                alt="Whatsapp"
+                width={70}
+                height={70}
+                className="mx-auto"
+              />
+              <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
+                <h4 align="center">Whatsapp</h4>
+              </h1>
+            </a>
+          </div>
+
+          <div className="rounded-md">
+            <a
+              href="https://re-craft.whats.bz/"
+              target="_blank"
+              rel="noopener noreferrer"
+              D
+            >
+              <img
+                src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/WcGegSln4UM6HcPkKnfN.png"
+                alt="Website"
+                width={70}
+                height={70}
+                className="mx-auto"
+              />
+              <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
+                <h4 align="center">Website</h4>
+              </h1>
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <h6 className="my-px px-1 sm:px-1 pb-24"></h6>
+        </div>
       </div>
       <Tabs />
-
-      <div>
-        <h6 className="my-px px-1 sm:px-1 pb-10 "></h6>
-      </div>
-
-      <img
-        src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/Em5mtzT5uYe5Fea8BORk.jpg"
-        alt="Landscape picture"
-        width={500}
-        height={500}
-        className="mx-auto"
-      />
-
-      <div>
-        <h1 className="text-lg my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black to-pink-600 font-bold">
-          <h4 align="center">One support, One reduce</h4>
-        </h1>
-      </div>
-      <div>
-        <h6 className="my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black text-justify mx-auto">
-          Let is reduce the fabric scrap from the craft production in Laos by
-          supporting the Re-craft products made from fabric scrap and everyone
-          also can promote reuse and recycling to society by sharing your
-          picture with Re-Craft products on social media.
-        </h6>
-      </div>
-      <br></br>
-      <div>
-        <h1 className="text-lg my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black to-pink-600 font-bold justify-center">
-          <h4 align="center">STAY CONNECTED</h4>
-        </h1>
-      </div>
-      <div>
-        <h6 className="my-px px-4 text-transparent bg-clip-text bg-gradient-to-br text-black text-justify mx-auto">
-          <h4 align="center">Join our community</h4>
-        </h6>
-      </div>
-
-      <div>
-        <h6 className="my-px px-1 sm:px-1 pb-1"></h6>
-      </div>
-
-      <div className="flex grid-cols-4 mx-auto flex-wrap justify-center">
-        <div className="rounded-md">
-          <a
-            href="https://www.facebook.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/6hs76UNVSk5usaPNztss.png"
-              alt="Facebook"
-              width={70}
-              height={70}
-              className="mx-auto "
-            />
-            <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
-              <h4 align="center">Facebook </h4>
-            </h1>
-          </a>
-        </div>
-
-        <div className="rounded-md">
-          <a
-            href="https://www.instagram.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/SiVGiQntQs6w9IfKJOrU.png"
-              alt="Instagram"
-              width={70}
-              height={70}
-              className="mx-auto"
-            />
-            <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
-              <h4 align="center">Instagram</h4>
-            </h1>
-          </a>
-        </div>
-
-        <div className="rounded-md">
-          <a
-            href="https://wa.me/+8562023564154"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/2PQrdSjHEah2N7wM4oGQ.png"
-              alt="Whatsapp"
-              width={70}
-              height={70}
-              className="mx-auto"
-            />
-            <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
-              <h4 align="center">Whatsapp</h4>
-            </h1>
-          </a>
-        </div>
-
-        <div className="rounded-md">
-          <a
-            href="https://re-craft.whats.bz/"
-            target="_blank"
-            rel="noopener noreferrer"
-            D
-          >
-            <img
-              src="https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/1VgavaYVt6bjm3vpJakt/pub/WcGegSln4UM6HcPkKnfN.png"
-              alt="Website"
-              width={70}
-              height={70}
-              className="mx-auto"
-            />
-            <h1 className="my-px px-2 text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-orange-600 font-medium">
-              <h4 align="center">Website</h4>
-            </h1>
-          </a>
-        </div>
-      </div>
-
-      <div>
-        <h6 className="my-px px-1 sm:px-1 pb-24"></h6>
-      </div>
     </>
   );
 }
--- pages/Products.js
+++ pages/Products.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Tabs from "../components/Tabs";
-import Footer from "../components/Footer";
 import CountCartStyles from "../styles/CountCart.module.css";
 import styles from "../styles/ListProducts.module.css";
 import CartProductstyles from "../styles/CartProducts.module.css";
 import ListProducts from "./ListProducts";
+import Footer from "../components/Footer";
 
 function Products() {
   return (
@@ -97,17 +97,18 @@ function Products() {
 
       <br />
       {/* Search Box end*/}
-      <div className="mx-auto place-content-center pl-auto justify-center ml-auto mr-auto">
-      <div className={CartProductstyles.Products}>
-        <ListProducts />
-      </div>
+      <div className="mx-auto place-content-center pl-auto justify-center ml-auto mr-auto w-full">
+        <div className={CartProductstyles.Width}>
+          <div className={CartProductstyles.Products}>
+            <ListProducts />
+          </div>
+          <Footer />
+        </div>
       </div>
 
       <div className="fixed top-0 left-0 right-77 inset-x-0">
         <Tabs />
       </div>
-
-      <Footer />
     </>
   );
 }
